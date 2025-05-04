5ff65759f3e3e7f63c5539a2e2a86831fc069cf2
phothinmg <phothinmg@gmail.com>
2025-05-04 11:49:49 +0630

    modified:   index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,20 @@
-import type { Root, Doctype, ElementContent, Element } from "hast";
+import type {
+  Root as IRoot,
+  Doctype as IDoctype,
+  ElementContent as IElementContent,
+  Element as IElement,
+  Node as INode,
+  Text as IText,
+  Comment as IComment,
+} from "hast";
+
+export interface Root extends IRoot {}
+export interface Doctype extends IDoctype {}
+export type ElementContent = IElementContent;
+export interface Node extends INode {}
+export interface Element extends IElement {}
+export interface Text extends IText {}
+export interface Comment extends IComment {}
 
 export type TextileVisitor = (
   node: Root | Doctype | ElementContent,
@@ -10,3 +26,5 @@ export interface TextileTreeWalker {
 }
 export interface TextileExtension extends TextileTreeWalker {}
 export type TextileExtensionFn = (...args: any[]) => TextileExtension;
+
+export declare class TextError {}
